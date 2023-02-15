1d719ef6aee8450ac93652e749402ee02ab13312
aleksejrevakin <alexey.amahasla@gmail.com>
2023-02-15 18:14:23 +0300

    Initial Commit

---
+++ Drink WaterUITests/Drink_WaterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Drink_WaterUITestsLaunchTests.swift
+//  Drink WaterUITests
+//
+//  Created by Алексей Ревякин on 15.02.2023.
+//
+
+import XCTest
+
+final class Drink_WaterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
