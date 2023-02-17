d6bd9e9ce54f0d244e4740ea4d682c5203efc7b0
aleksejrevakin <alexey.amahasla@gmail.com>
2023-02-17 14:00:04 +0300

    Create new Color and Font

--- Drink Water/Resources + Extension/Resources.swift
+++ Drink Water/Resources + Extension/Resources.swift
@@ -16,9 +16,10 @@ enum Resources {
     
     enum Color {
         static let appGreen = UIColor(red: 39/255, green: 237/255, blue: 130/255, alpha: 1)
+        static let whiteClear = UIColor(white: 1, alpha: 0.8)
     }
     enum Fonts {
         static let drunkWaterFont = UIFont.systemFont(ofSize: 35, weight: .bold)
-        static let needToDringWaterFont = UIFont.systemFont(ofSize: 15)
+        static let needToDringWaterFont = UIFont.systemFont(ofSize: 17)
     }
 }
