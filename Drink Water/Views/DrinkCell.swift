8cfe18b50cb2fe986fd31b790d271d3473b48050
aleksejrevakin <alexey.amahasla@gmail.com>
2023-02-17 14:00:45 +0300

    Create Drink Cell and UI Elemen for cel

--- Drink Water/Views/DrinkCell.swift
+++ Drink Water/Views/DrinkCell.swift
@@ -8,5 +8,30 @@
 import UIKit
 
 class DrinkCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+       let imageView = UIImageView()
+        return imageView
+    }()
+    private let sizeLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initialize(){
+        
+    }
     
 }
